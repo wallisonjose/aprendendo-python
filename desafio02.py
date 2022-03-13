12a65c594d4ad416ddbb93f26437f7ac436f2b0e
Wallison <wallisonjose22@outlook.com>
2022-03-12 23:25:10 -0300

    Primeiro Commit - Primeiros Exercícios feitos durante o curso de phyton

---
+++ desafio02.py
@@ -0,0 +1,4 @@
+dia = input('Qual dia do mês você nasceu? ')
+mes = input('Qual mês você nasceu? ')
+ano = input('Em que ano você nasceu? ')
+print('Você nasceu no dia', dia, 'de', mes, 'de', ano, 'certo?')
---
+++ desafio03.py
@@ -0,0 +1,7 @@
+# todo input é recebido como string
+# Primeira forma de converter um input em int de forma direta
+num1 = int(input('Digite um número: '))
+# Segunda forma de converter um input em int em 2 passos
+num2 = input('Digite outro número: ')
+num2 = int(num2)
+print( 'A soma entre', num1, 'e', num2, 'é', num1+num2 )
